c6b969560d9d8086b70c30043d6d10418780c406
Oliver Lee <oliverzlee@gmail.com>
2015-09-11 16:21:49 +0200

    Move Bicycle member function definitions
    Move Bicycle member function definitions after entire class is declared.

--- bicycle.h
+++ bicycle.h
@@ -39,62 +39,26 @@ class Bicycle{
         state_t x_integrate(const state_t& x, double dt) const;
         output_t y(const state_t& x) const;
         void set_v(double v, double dt = 0.0);
-        void set_A(const state_matrix_t& A) {
-            m_A = A;
-        }
-        void set_B(const input_matrix_t& B) {
-            m_B = B;
-        }
-        void set_C(const output_matrix_t& C) {
-            m_C = C;
-        }
-        void set_D(const feedthrough_matrix_t& D) {
-            m_D = D;
-        }
+        void set_A(const state_matrix_t& A);
+        void set_B(const input_matrix_t& B);
+        void set_C(const output_matrix_t& C);
+        void set_D(const feedthrough_matrix_t& D);
 
         // (pseudo) parameter accessors
-        state_matrix_t A() const {
-            return m_A;
-        }
-        input_matrix_t B() const {
-            return m_B;
-        }
-        output_matrix_t C() const {
-            return m_C;
-        }
-        feedthrough_matrix_t D() const {
-            return m_D;
-        }
-        state_matrix_t Ad() const {
-            return m_Ad;
-        }
-        input_matrix_t Bd() const {
-            return m_Bd;
-        }
-        output_matrix_t Cd() const {
-            return m_C;
-        }
-        feedthrough_matrix_t Dd() const {
-            return m_D;
-        }
-        second_order_matrix_t M() const {
-            return m_M;
-        }
-        second_order_matrix_t C1() const {
-            return m_C1;
-        }
-        second_order_matrix_t K0() const {
-            return m_K0;
-        }
-        second_order_matrix_t K2() const {
-            return m_K2;
-        }
-        double v() const {
-            return m_v;
-        }
-        double dt() const {
-            return m_dt;
-        }
+        state_matrix_t A() const;
+        input_matrix_t B() const;
+        output_matrix_t C() const;
+        feedthrough_matrix_t D() const;
+        state_matrix_t Ad() const;
+        input_matrix_t Bd() const;
+        output_matrix_t Cd() const;
+        feedthrough_matrix_t Dd() const;
+        second_order_matrix_t M() const;
+        second_order_matrix_t C1() const;
+        second_order_matrix_t K0() const;
+        second_order_matrix_t K2() const;
+        double v() const;
+        double dt() const;
 
     private:
         double m_v; // parameterized forward speed
@@ -128,5 +92,61 @@ class Bicycle{
             state_t, double, state_t, double,
             boost::numeric::odeint::vector_space_algebra> m_stepper_noinput;
 }; // class Bicycle
+
+// define simple member functions using inline
+inline void Bicycle::set_A(const state_matrix_t& A) {
+    m_A = A;
+}
+inline void Bicycle::set_B(const input_matrix_t& B) {
+    m_B = B;
+}
+inline void Bicycle::set_C(const output_matrix_t& C) {
+    m_C = C;
+}
+inline void Bicycle::set_D(const feedthrough_matrix_t& D) {
+    m_D = D;
+}
+inline Bicycle::state_matrix_t Bicycle::A() const {
+    return m_A;
+}
+inline Bicycle::input_matrix_t Bicycle::B() const {
+    return m_B;
+}
+inline Bicycle::output_matrix_t Bicycle::C() const {
+    return m_C;
+}
+inline Bicycle::feedthrough_matrix_t Bicycle::D() const {
+    return m_D;
+}
+inline Bicycle::state_matrix_t Bicycle::Ad() const {
+    return m_Ad;
+}
+inline Bicycle::input_matrix_t Bicycle::Bd() const {
+    return m_Bd;
+}
+inline Bicycle::output_matrix_t Bicycle::Cd() const {
+    return m_C;
+}
+inline Bicycle::feedthrough_matrix_t Bicycle::Dd() const {
+    return m_D;
+}
+inline Bicycle::second_order_matrix_t Bicycle::M() const {
+    return m_M;
+}
+inline Bicycle::second_order_matrix_t Bicycle::C1() const {
+    return m_C1;
+}
+inline Bicycle::second_order_matrix_t Bicycle::K0() const {
+    return m_K0;
+}
+inline Bicycle::second_order_matrix_t Bicycle::K2() const {
+    return m_K2;
+}
+inline double Bicycle::v() const {
+    return m_v;
+}
+inline double Bicycle::dt() const {
+    return m_dt;
+}
 
 } // namespace model
