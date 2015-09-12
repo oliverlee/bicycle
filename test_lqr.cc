54e68a9f577222e8cb4e59419ec80dc3bbfde0d5
Oliver Lee <oliverzlee@gmail.com>
2015-09-12 03:57:58 +0200

    Add timing to Lqr test
    Compare time duration for simulation with and without computing control
    input.

--- test_lqr.cc
+++ test_lqr.cc
@@ -1,4 +1,5 @@
 #include <array>
+#include <chrono>
 #include <iostream>
 #include <boost/math/constants/constants.hpp>
 #include "bicycle.h"
@@ -14,7 +15,7 @@ namespace {
     const double as_radians = boost::math::constants::degree<double>(); // convert degrees to radians
     const double as_degrees = boost::math::constants::radian<double>(); // convert radians to degrees
 
-    model::Bicycle::state_t x(
+    const model::Bicycle::state_t x0(
             (model::Bicycle::state_t() <<
                 0, 10, 10, 0
                 ).finished() * as_radians
@@ -34,6 +35,8 @@ namespace {
 
     std::array<model::Bicycle::state_t, N> system_state;
     std::array<model::Bicycle::input_t, N> system_control_input;
+
+    std::chrono::time_point<std::chrono::system_clock> start, stop;
 } // namespace
 
 int main(int argc, char* argv[]) {
@@ -42,21 +45,42 @@ int main(int argc, char* argv[]) {
     model::Bicycle bicycle(argv[1], v0, dt);
     controller::Lqr<model::Bicycle> lqr(bicycle, Q, R, xt, n);
 
-    std::cout << "initial state: [" << x.transpose() * as_degrees << "]' deg" << std::endl;
+    std::cout << "initial state: [" << x0.transpose() * as_degrees << "]' deg" << std::endl;
 
+    std::cout << std::endl << "simulating without controller..." << std::endl;
     auto it_x = system_state.begin();
-    auto it_u = system_control_input.begin();
-
-    std::cout << std::endl << "simulating..." << std::endl;
-    for (; it_x != system_state.end(); ++it_x, ++it_u) {
-        *it_u = lqr.control_calculate(x);
-        x = bicycle.x_next(x, *it_u);
-        *it_x = x;
-//        std::cout << x.transpose() * as_degrees << std::endl;
+    xt = x0;;
+    start = std::chrono::system_clock::now();
+    for (; it_x != system_state.end(); ++it_x) {
+        xt = bicycle.x_next(xt);
+        *it_x = xt;
     }
+    stop = std::chrono::system_clock::now();
+    std::chrono::duration<double> duration = stop - start;
+    std::cout << "duration for simulation without controller: " <<
+        std::chrono::duration_cast<std::chrono::microseconds>(duration).count() <<
+        " us" << std::endl;
+
+
+    std::cout << std::endl << "simulating with controller..." << std::endl;
+    it_x = system_state.begin();
+    auto it_u = system_control_input.begin();
+    xt = x0;
+    start = std::chrono::system_clock::now();
+    for (; it_x != system_state.end(); ++it_x, ++it_u) {
+        *it_u = lqr.control_calculate(xt);
+        xt = bicycle.x_next(xt, *it_u);
+        *it_x = xt;
+//        std::cout << xt.transpose() * as_degrees << std::endl;
+    }
+    stop = std::chrono::system_clock::now();
+    duration = stop - start;
+    std::cout << "duration for simulation with controller: " <<
+        std::chrono::duration_cast<std::chrono::microseconds>(duration).count() <<
+        " us" << std::endl;
 
     std::cout << "state at end of simulation (" << N << " steps @ " << fs << " Hz)" << std::endl;
-    std::cout << "final state: [" << x.transpose() * as_degrees << "]' deg" << std::endl;
+    std::cout << "final state: [" << system_state.back().transpose() * as_degrees << "]' deg" << std::endl;
     std::cout << "final control input: [" << system_control_input.back().transpose() << "]'" << std::endl;
 
     return EXIT_SUCCESS;
