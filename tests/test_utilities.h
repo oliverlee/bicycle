fdeab35e4997c16515efea32d97845786d373990
Oliver Lee <oliverzlee@gmail.com>
2015-09-13 19:43:58 +0200

    Add allclose function for testing
    Compare relative and absolute tolerence of matrix elements. Eigen
    isApprox() functions compare matrix norms.

--- tests/test_utilities.h
+++ tests/test_utilities.h
@@ -8,3 +8,20 @@ std::string output_matrices(Eigen::MatrixXd expected, Eigen::MatrixXd actual) {
     ss << "expected:\n" << expected << "\nactual:\n" << actual << std::endl;
     return ss.str();
 }
+
+/*
+ * allclose() function to match numpy.allclose
+ * https://stackoverflow.com/questions/15051367/how-to-compare-vectors-approximately-in-eige://stackoverflow.com/questions/15051367/how-to-compare-vectors-approximately-in-eigen
+ */
+namespace test {
+
+template<typename DerivedA, typename DerivedB>
+bool allclose(const Eigen::DenseBase<DerivedA>& a, const Eigen::DenseBase<DerivedB>& b,
+              const typename DerivedA::RealScalar& rtol
+                = Eigen::NumTraits<typename DerivedA::RealScalar>::dummy_precision(),
+              const typename DerivedA::RealScalar& atol
+                = Eigen::NumTraits<typename DerivedA::RealScalar>::epsilon()) {
+  return ((a.derived() - b.derived()).array().abs() <= (atol + rtol * b.derived().array().abs())).all();
+}
+
+} // namespace test
