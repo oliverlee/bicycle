81c321ef9527fb242719c2d92c5957ba8a9c4b74
Oliver Lee <oliverzlee@gmail.com>
2015-09-15 13:18:38 +0200

    Parameterize Kalman convergence test by speed
    Parameterize Kalman convergence test by speed, from 0.5 m/s to 9.0 m/s.
    Allow each state vector element different tolerances.

--- tests/test_kalman.cc
+++ tests/test_kalman.cc
@@ -5,54 +5,81 @@
 #include "parameters.h"
 #include "test_utilities.h"
 
-// TODO: Add more extensive testing
-namespace {
-    const double reltol = 1e-4;
-    const double abstol = 1e-8;
-    const double dt = 1.0/200;
-    const double v = 4.0;
-    const uint32_t N = 1000; // simulation length
+class KalmanConvergenceTest: public ::testing::TestWithParam<double> {
+    public:
+        virtual void SetUp() {
+            bicycle = new model::Bicycle(parameters::benchmark::M, parameters::benchmark::C1,
+                    parameters::benchmark::K0, parameters::benchmark::K2, GetParam(), dt);
+            bicycle->set_C(parameters::defaultvalue::bicycle::C);
+            kalman = new observer::Kalman<model::Bicycle>(*bicycle,
+                    parameters::defaultvalue::kalman::Q,
+                    parameters::defaultvalue::kalman::R,
+                    model::Bicycle::state_t::Zero(),
+                    parameters::defaultvalue::kalman::P0);
+            gen = std::mt19937(rd());
+            r0 = std::normal_distribution<>(0, parameters::defaultvalue::kalman::R(0, 0));
+            r1 = std::normal_distribution<>(0, parameters::defaultvalue::kalman::R(1, 1));
 
-    model::Bicycle::state_t x(
-            model::Bicycle::state_t::Zero());
+            // set initial state
+            x << 0, 10, 10, 0; // define x in degrees
+            x *= constants::as_radians;
+        }
 
-    const model::Bicycle::output_matrix_t C = parameters::defaultvalue::bicycle::C;
+        virtual void TearDown() {
+            delete bicycle;
+            delete kalman;
+            bicycle = nullptr;
+            kalman = nullptr;
+        }
 
-    const observer::Kalman<model::Bicycle>::error_covariance_t P0 =
-        parameters::defaultvalue::kalman::P0;
-    const observer::Kalman<model::Bicycle>::process_noise_covariance_t Q =
-        parameters::defaultvalue::kalman::Q;
-    const observer::Kalman<model::Bicycle>::measurement_noise_covariance_t R =
-        parameters::defaultvalue::kalman::R;
+        void simulate() {
+            for(unsigned int i = 0; i < N; ++i) {
+                x = bicycle->x_next(x);
 
-    std::random_device rd; // used only to seed rng
-} // namespace
+                auto z = bicycle->y(x);
+                z(0) += r0(gen);
+                z(1) += r1(gen);
 
-TEST(Kalman, Convergence) {
-    std::mt19937 gen(rd());
-    std::normal_distribution<> r0(0, R(0, 0));
-    std::normal_distribution<> r1(0, R(1, 1));
+                kalman->time_update();
+                kalman->measurement_update(z);
+            }
+        }
 
-    model::Bicycle bicycle(parameters::benchmark::M, parameters::benchmark::C1,
-            parameters::benchmark::K0, parameters::benchmark::K2, v, dt);
-    bicycle.set_C(C);
-    // set x estimate to zero in Kalman filter
-    observer::Kalman<model::Bicycle> kalman(bicycle, Q, R, x, P0);
+    protected:
+        const double roll_tol = 1 * constants::as_radians;
+        const double steer_tol = 0.1 * constants::as_degrees;
+        const double roll_rate_tol = 10 * roll_tol * constants::as_radians;
+        const double steer_rate_tol = steer_tol * constants::as_degrees;
+        const double dt = 1.0/200;
+        const uint32_t N = 1000; // simulation length
+        model::Bicycle* bicycle;
+        observer::Kalman<model::Bicycle>* kalman;
+        model::Bicycle::state_t x;
 
-    // set initial state
-    x << 0, 10, 10, 0; // define x in degrees
+        const observer::Kalman<model::Bicycle>::error_covariance_t P0 =
+            parameters::defaultvalue::kalman::P0;
+        const observer::Kalman<model::Bicycle>::process_noise_covariance_t Q =
+            parameters::defaultvalue::kalman::Q;
+        const observer::Kalman<model::Bicycle>::measurement_noise_covariance_t R =
+            parameters::defaultvalue::kalman::R;
 
-    for(unsigned int i = 0; i < N; ++i) {
-        x = bicycle.x_next(x);
+        std::random_device rd; // used only to seed rng
+        std::mt19937 gen;
+        std::normal_distribution<> r0;
+        std::normal_distribution<> r1;
+};
 
-        auto z = bicycle.y(x);
-        z(0) += r0(gen);
-        z(1) += r1(gen);
-
-        kalman.time_update();
-        kalman.measurement_update(z);
-    }
-
-    EXPECT_TRUE(test::allclose(kalman.x(), x, reltol, abstol)) << output_matrices(kalman.x().transpose(), x.transpose());
+TEST_P(KalmanConvergenceTest, ZeroInput) {
+    simulate();
+    auto actual = kalman->x();
+    auto expected = x;
+    EXPECT_NEAR(actual(0), expected(0), roll_tol);
+    EXPECT_NEAR(actual(1), expected(1), steer_tol);
+    EXPECT_NEAR(actual(2), expected(2), roll_rate_tol);
+    EXPECT_NEAR(actual(3), expected(3), steer_rate_tol);
 }
 
+INSTANTIATE_TEST_CASE_P(
+    ConvergenceRange_1_9,
+    KalmanConvergenceTest,
+    ::testing::Range(0.5, 9.5, 0.5));
