aaa5b192b95eaa3191436a90c55755875d46086e
Oliver Lee <oliverzlee@gmail.com>
2015-09-18 20:22:08 +0200

    Create generated files in generated build directory

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -4,12 +4,12 @@ function(FLATBUFFERS_GENERATE_C_HEADERS Name)
         get_filename_component(FLATC_OUTPUT ${FILE} NAME_WE)
         get_filename_component(SCHEMA_DIRECTORY ${FILE} DIRECTORY)
         set(FLATC_OUTPUT
-          "${CMAKE_CURRENT_BINARY_DIR}/${FLATC_OUTPUT}_generated.h")
+            "${PROJECT_BINARY_DIR}/generated/${FLATC_OUTPUT}_generated.h")
         list(APPEND FLATC_OUTPUTS ${FLATC_OUTPUT})
 
         add_custom_command(OUTPUT ${FLATC_OUTPUT}
           COMMAND flatc
-          ARGS -c -o "${CMAKE_CURRENT_BINARY_DIR}/" ${FILE}
+          ARGS -c -o "${PROJECT_BINARY_DIR}/generated/" ${FILE}
           COMMENT "Building C++ header for ${FILE}"
           WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
     endforeach()
