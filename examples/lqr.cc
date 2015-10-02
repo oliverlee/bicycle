4b6df2f310f007537705f94c8f7b529f39bc34a2
Oliver Lee <oliverzlee@gmail.com>
2015-10-02 18:45:51 +0200

    Add option to reduce LQR control input size
    A system may have an input size of 'm', but a realizable control input
    size less than 'm'. This can be set with the LQR input weight matrix
    'R'. By setting a diagonal entry of 'R' to zero, the corresponding entry
    in 'u', the control input vector, will always set to zero. This process
    reduces the size of the system input in all LQR computations.
    
    For example, with 'm' = 4 and
        R = [ *, 0, 0, 0 ]
            [ 0, 0, 0, 0 ]
            [ 0, 0, 0, 0 ]
            [ 0, 0, 0, * ]
    
    The LQR controller will always return a control input vector
        u' = [ *, 0, 0, * ]

--- examples/lqr.cc
+++ examples/lqr.cc
@@ -6,19 +6,26 @@
 #include "parameters.h"
 
 namespace {
+    using bicycle_t = model::Bicycle;
+    using lqr_t = controller::Lqr<bicycle_t>;
+
     const double fs = 200; // sample rate [Hz]
     const double dt = 1.0/fs; // sample time [s]
-    const double v0 = 4.0; // forward speed [m/s]
+    const double v0 = 4.1; // forward speed [m/s]
     const size_t N = 1000; // length of simulation in samples
     const size_t n = 100;  // length of horizon in samples
 
-    const model::Bicycle::state_t x0(
-            (model::Bicycle::state_t() <<
+    const bicycle_t::state_t x0(
+            (bicycle_t::state_t() <<
                 0, 0, 10, 10, 0).finished() * constants::as_radians);
-    model::Bicycle::state_t x;
+    bicycle_t::state_t x;
 
-    std::array<model::Bicycle::state_t, N> system_state;
-    std::array<model::Bicycle::input_t, N> system_control_input;
+    //constexpr double r0 = 10; // roll torque cost weight
+    constexpr double r0 = 0.0; // roll torque cost weight
+    constexpr double r1 = 1.0; // steer torque cost weight
+
+    std::array<bicycle_t::state_t, N> system_state;
+    std::array<bicycle_t::input_t, N> system_control_input;
 
     std::chrono::time_point<std::chrono::system_clock> start, stop;
 } // namespace
@@ -27,16 +34,18 @@ int main(int argc, char* argv[]) {
     (void)argc;
     (void)argv;
 
-    model::Bicycle bicycle(parameters::benchmark::M, parameters::benchmark::C1,
+    bicycle_t bicycle(parameters::benchmark::M, parameters::benchmark::C1,
             parameters::benchmark::K0, parameters::benchmark::K2,
             parameters::benchmark::wheelbase,
             parameters::benchmark::trail,
             parameters::benchmark::steer_axis_tilt,
             v0, dt);
-    controller::Lqr<model::Bicycle> lqr(bicycle,
-            controller::Lqr<model::Bicycle>::state_cost_t::Identity(),
-            0.1 * controller::Lqr<model::Bicycle>::input_cost_t::Identity(),
-            model::Bicycle::state_t::Zero(), n);
+    lqr_t lqr(bicycle,
+            lqr_t::state_cost_t::Identity(),
+            (lqr_t::input_cost_t() <<
+             r0,  0,
+              0, r1).finished(),
+            bicycle_t::state_t::Zero(), n);
 
     x = x0;
     std::cout << "initial state: [" << x.transpose() * constants::as_degrees << "]' deg" << std::endl;
@@ -52,6 +61,9 @@ int main(int argc, char* argv[]) {
     std::cout << "duration for simulation without controller: " <<
         std::chrono::duration_cast<std::chrono::microseconds>(duration).count() <<
         " us" << std::endl;
+
+    std::cout << "state at end of simulation (" << N << " steps @ " << fs << " Hz)" << std::endl;
+    std::cout << "final state: [" << system_state.back().transpose() * constants::as_degrees << "]' deg" << std::endl;
 
     x = x0;
     std::cout << std::endl << "simulating with controller..." << std::endl;
--- inc/lqr.h
+++ inc/lqr.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <type_traits>
 #include <Eigen/Core>
+#include <Eigen/QR>
 #include "discrete_linear.h"
 
 namespace controller {
@@ -17,12 +18,10 @@ class Lqr {
         using lqr_gain_t = typename Eigen::Matrix<double, T::m, T::n>;
         using state_cost_t = state_matrix_t;
         using input_cost_t = typename Eigen::Matrix<double, T::m, T::m>;
+        using control_mask_t = typename Eigen::Matrix<uint32_t, T::m, 1>;
 
         Lqr(T& system, const state_cost_t& Q, const input_cost_t& R,
-                const state_t& r, uint32_t horizon_iterations) :
-            m_system(system), m_horizon(horizon_iterations),
-            m_r(r), m_Q(Q), m_R(R),
-            m_steady_state(false), m_Ad(system.Ad()), m_Bd(system.Bd()) { }
+                const state_t& r, uint32_t horizon_iterations);
 
         input_t control_calculate(const state_t& x);
         input_t control_calculate(const state_t& x, const state_t& r);
@@ -45,20 +44,26 @@ class Lqr {
         double dt() const;
 
     private:
-        T& m_system;
-        uint32_t m_horizon;
-        state_t m_r; // reference state
-        lqr_gain_t m_K;
-        state_cost_t m_P; // cost-to-go
-        state_cost_t m_Q;
-        input_cost_t m_R;
-        bool m_steady_state;
-        state_matrix_t m_Ad;
-        input_matrix_t m_Bd;
+        T& m_system;                    // controlled system or plant
+        uint32_t m_horizon;             // horizon length in iterations
+        state_t m_r;                    // reference state
+        lqr_gain_t m_K;                 // computed feedback gain
+        state_cost_t m_P;               // cost-to-go
+        state_cost_t m_Q;               // state cost weights
+        input_cost_t m_R;               // input cost weights
+        bool m_steady_state;            // steady state indicator
+        state_matrix_t m_Ad;            // copy of system state matrix
+        input_matrix_t m_Bd;            // copy of system input matrix
+        control_mask_t m_mask;          // control input mask
+        uint32_t m_m;                   // number of accessible control inputs
+        input_cost_t m_Rr;              // reduced form of input cost matrix
+        input_matrix_t m_Br;            // reduced form of input matrix
 
         void perform_value_iteration();
         void update_lqr_gain();
         void update_horizon_cost();
+        void set_control_mask();
+        void reduce_input_matrices();
 }; // class Lqr
 
 template<typename T>
