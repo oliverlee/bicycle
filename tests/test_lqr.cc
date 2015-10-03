2d09167a56dad2b3053e5c8c9363dbbe0add75ba
Oliver Lee <oliverzlee@gmail.com>
2015-10-03 17:38:05 +0200

    Add LQR tests with roll torque disturbance

--- tests/test_lqr.cc
+++ tests/test_lqr.cc
@@ -20,7 +20,7 @@ class LqrTrackingTest: public ConvergenceTest {
         }
 };
 
-TEST_P(LqrTrackingTest, ZeroReferenceWithRollTorque) {
+TEST_P(LqrTrackingTest, ZeroReferenceWithRollTorqueControl) {
     m_lqr->set_R((lqr_t::input_cost_t() <<
             0.1,   0,
               0, 0.1).finished());
@@ -33,7 +33,37 @@ TEST_P(LqrTrackingTest, ZeroReference) {
     test_state_near(x_true(), bicycle_t::state_t::Zero());
 }
 
+TEST_P(LqrTrackingTest, ZeroReferenceWithPeriodicRollTorqueDisturbance) {
+    simulate();
+
+    for(unsigned int i = 0; i < m_N; ++i) {
+        auto u = m_lqr->control_calculate(m_x);
+
+        bool disturb = ((i / 100) % 5 == 0);
+        if (disturb) {
+            u[0] += 1.0;
+        }
+
+        m_x = m_bicycle->x_next(m_x, u);
+    }
+
+    test_state_near(x_true(), bicycle_t::state_t::Zero());
+}
+
+TEST_P(LqrTrackingTest, ZeroReferenceWithConstantRollTorqueDisturbance) {
+    simulate();
+
+    for(unsigned int i = 0; i < m_N; ++i) {
+        auto u = m_lqr->control_calculate(m_x);
+        u[0] += 1.0;
+
+        m_x = m_bicycle->x_next(m_x, u);
+    }
+
+    test_state_near(x_true(), bicycle_t::state_t::Zero());
+}
+
 INSTANTIATE_TEST_CASE_P(
-    ConvergenceRange_1_9,
+    TrackingRange_1_9,
     LqrTrackingTest,
     ::testing::Range(0.5, 9.5, 0.5));
