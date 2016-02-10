0a53feaacee2f8438b718f1ea47e8d15afe91324
Oliver Lee <oliverzlee@gmail.com>
2016-02-10 15:43:48 +0100

    Modify UDP example to use PeriodicTransmitServer

--- examples/udp.cc
+++ examples/udp.cc
@@ -11,8 +11,36 @@ namespace {
     const double dt = 1.0/fs; // sample time [s]
     const double v0 = 4.0; // forward speed [m/s]
     const size_t N = 1000; // length of simulation in samples
+    const auto simulation_period = std::chrono::microseconds(100);
+    const auto transmission_period = std::chrono::milliseconds(1);
 
     std::array<model::Bicycle::state_t, N> discrete_time_system_state_n;
+    std::array<int32_t, N> simulation_loop_period;
+    std::array<int32_t, N> transmit_loop_period;
+
+    void update(const asio::error_code& e, asio::high_resolution_timer* timer,
+            int* count, model::Bicycle* bicycle, model::Bicycle::state_t* x,
+            std::chrono::high_resolution_clock::time_point time) {
+        if (*count < N) {
+            *x = bicycle->x_next(*x);
+
+            auto now = std::chrono::high_resolution_clock::now();
+            auto dt = std::chrono::duration_cast<std::chrono::microseconds>(now - time).count();
+
+            simulation_loop_period[*count] = dt; // store calculated loop period
+            discrete_time_system_state_n[(*count)++] = *x; // store system state
+
+            timer->expires_at(timer->expires_at() + simulation_period);
+            timer->async_wait(std::bind(update,
+                        std::placeholders::_1,
+                        timer,
+                        count,
+                        bicycle,
+                        x,
+                        now));
+
+        }
+    }
 } // namespace
 
 int main(int argc, char* argv[]) {
@@ -24,12 +52,47 @@ int main(int argc, char* argv[]) {
     x << 0, 0, 10, 10, 0; // define in degrees
     x *= constants::as_radians;
 
-    network::udp::Server server;
+    using clock = std::chrono::high_resolution_clock;
+    auto start = clock::now();
+    auto i = transmit_loop_period.begin();
 
-    for (auto& state: discrete_time_system_state_n) {
-        state = bicycle.x_next(x);
-        x = state;
-        server.async_send(reinterpret_cast<uint8_t*>(x.data()), x.size()*sizeof(double));
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    // periodic transmit buffer function
+    auto f = [&x, &i, &start]() -> asio::const_buffer {
+        auto now = clock::now();
+        *i++ = std::chrono::duration_cast<std::chrono::microseconds>(now - start).count();
+        start = now;
+        return asio::const_buffer(static_cast<void*>(x.data()), x.size()*sizeof(double));
+    };
+
+    network::udp::PeriodicTransmitServer<decltype(f)> server(
+            network::udp::default_port, transmission_period, std::cref(f));
+
+    asio::io_service io_service;
+    asio::high_resolution_timer simulation_timer(io_service, simulation_period);
+    int count = 0;
+
+    simulation_timer.async_wait(std::bind(update,
+                std::placeholders::_1,
+                &simulation_timer,
+                &count,
+                &bicycle,
+                &x,
+                start));
+
+    io_service.run();
+
+    std::cout << "simulation loop periods (us): ";
+    for (auto dt: simulation_loop_period) {
+        std::cout << dt << " ";
     }
+
+    std::cout << "\n";
+    std::cout << "transmission loop periods (us): ";
+    for (auto dt: transmit_loop_period) {
+        if (dt == 0) {
+            break;
+        }
+        std::cout << dt << " ";
+    }
+    std::cout << "\n";
 }
