e8cd31516bff3b77f7a8a21191e02b9d7fd04ee7
Oliver Lee <oliverzlee@gmail.com>
2016-11-08 11:55:55 +0100

    Use constexpr when defining math constants

--- inc/constants.h
+++ inc/constants.h
@@ -4,8 +4,8 @@
 
 namespace constants {
     constexpr model::real_t g = 9.80665; // gravitational constant [m/s^2]
-    const model::real_t as_radians = boost::math::constants::degree<model::real_t>(); // convert degrees to radians
-    const model::real_t as_degrees = boost::math::constants::radian<model::real_t>(); // convert radians to degrees
-    const model::real_t two_pi = boost::math::constants::two_pi<model::real_t>();
-    const model::real_t pi = boost::math::constants::pi<model::real_t>();
+    constexpr model::real_t as_radians = boost::math::constants::degree<model::real_t>(); // convert degrees to radians
+    constexpr model::real_t as_degrees = boost::math::constants::radian<model::real_t>(); // convert radians to degrees
+    constexpr model::real_t two_pi = boost::math::constants::two_pi<model::real_t>();
+    constexpr model::real_t pi = boost::math::constants::pi<model::real_t>();
 } // namespace constants
